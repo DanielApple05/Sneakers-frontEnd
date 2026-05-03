30ff6de712ef3f59356a26005e690addb1cf884e
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-05-03 01:42:12 +0100

    Refactor code structure for improved readability and maintainability

--- sneakers-frontend/src/assets/components/contactComponent/contact.jsx
+++ sneakers-frontend/src/assets/components/contactComponent/contact.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import Header from '../headerComponent/header'
+import Header from '../headerComponent/header';
+import contactHero from '../../images/contactHeroBg.jpg';
 
 const contact = () => {
   return (
     <div>
       <Header />
-      <div>
+      <div className='relative' > 
+        <img
+          src={contactHero}
+          className='object-cover w-full h-100'
+          alt="" />
+        <div className='absolute top-0 p-15 font-bold '>
+          <p className=''>
+            We're Here 
+          </p>
+          <h1 className='tracking-widest'>To Help</h1>
+          <p className='font-semibold text-xs'>
+            Have a question or
+          </p>
+          <h3 className='font-bold'>
+            Contact Us
+          </h3>
+        </div>
+      </div>
+      <div className='flex'>
         <div>
           <div>
             <h3>
